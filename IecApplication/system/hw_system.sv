99c6f7c0747c3f7524de714f543bd6dbc4ce4445
Hannesk1973 <hannesk1973@gmail.com>
2019-04-17 20:40:28 +0200

    Redesign

---
+++ IecApplication/system/hw_system.sv
@@ -0,0 +1,195 @@
+%SYSTEMVAR_DECL
+  FUAI1 : REAL
+    ;
+ FUX1 : BOOL
+    ;
+ FUX2 : BOOL
+    ;
+ KeyGreen : BOOL
+    ;
+ KeyRed : BOOL
+    ;
+ SafetySwitch1 : BOOL
+    ;
+ ModeSwitchP1 : BOOL
+    ;
+ ModeSwitchP3 : BOOL
+    ;
+ LedRed : BOOL
+    ;
+ LedGreen : BOOL
+    ;
+ FUAO1 : REAL
+    ;
+%END
+
+
+
+#END_OF_IEC_PART
+
+@Puma @IecEditor 6 177 @Sve 25 
+@@@BEG_Comment@@@
+
+@@@END_Comment@@@
+
+@BEG_Contents 
+
+@BEG_Export 
+@RT(16)SveTreeContainer 
+11 
+@SysVar @RT(5)FUAI1 @RT(0) @T @F @DT @RT(4)REAL @RT(0) @T @T @BASIC 0 @F 
+@F @F 
+@T 
+@BEG_Attrib 
+4 @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@END_Attrib 
+0 
+
+
+@SysVar @RT(4)FUX1 @RT(0) @T @F @DT @RT(4)BOOL @RT(0) @T @T @BASIC 0 @F 
+@F @F 
+@T 
+@BEG_Attrib 
+4 @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@END_Attrib 
+0 
+
+
+@SysVar @RT(4)FUX2 @RT(0) @T @F @DT @RT(4)BOOL @RT(0) @T @T @BASIC 0 @F 
+@F @F 
+@T 
+@BEG_Attrib 
+4 @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@END_Attrib 
+0 
+
+
+@SysVar @RT(8)KeyGreen @RT(0) @T @F @DT @RT(4)BOOL @RT(0) @T @T @BASIC 0 @F 
+@F @F 
+@T 
+@BEG_Attrib 
+4 @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@END_Attrib 
+0 
+
+
+@SysVar @RT(6)KeyRed @RT(0) @T @F @DT @RT(4)BOOL @RT(0) @T @T @BASIC 0 @F 
+@F @F 
+@T 
+@BEG_Attrib 
+4 @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@END_Attrib 
+0 
+
+
+@SysVar @RT(13)SafetySwitch1 @RT(0) @T @F @DT @RT(4)BOOL @RT(0) @T @T @BASIC 0 @F 
+@F @F 
+@T 
+@BEG_Attrib 
+4 @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@END_Attrib 
+0 
+
+
+@SysVar @RT(12)ModeSwitchP1 @RT(0) @T @F @DT @RT(4)BOOL @RT(0) @T @T @BASIC 0 @F 
+@F @F 
+@T 
+@BEG_Attrib 
+4 @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@END_Attrib 
+0 
+
+
+@SysVar @RT(12)ModeSwitchP3 @RT(0) @T @F @DT @RT(4)BOOL @RT(0) @T @T @BASIC 0 @F 
+@F @F 
+@T 
+@BEG_Attrib 
+4 @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@END_Attrib 
+0 
+
+
+@SysVar @RT(6)LedRed @RT(0) @T @F @DT @RT(4)BOOL @RT(0) @T @T @BASIC 0 @F 
+@F @F 
+@T 
+@BEG_Attrib 
+4 @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@END_Attrib 
+0 
+
+
+@SysVar @RT(8)LedGreen @RT(0) @T @F @DT @RT(4)BOOL @RT(0) @T @T @BASIC 0 @F 
+@F @F 
+@T 
+@BEG_Attrib 
+4 @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@END_Attrib 
+0 
+
+
+@SysVar @RT(5)FUAO1 @RT(0) @T @F @DT @RT(4)REAL @RT(0) @T @T @BASIC 0 @F 
+@F @F 
+@T 
+@BEG_Attrib 
+4 @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@RT(0) @RT(0) 
+@RT(0) @RT(0) @RT(0) @RT(0) @RT(0) 
+@END_Attrib 
+0 
+
+
+@RT(15)SETreeContainer 
+0 
+@RT(15)SOTreeContainer 
+0 
+
+@RT(15)SATreeContainer 
+0 
+
+@RT(21)SExtAttrTreeContainer 
+0 
+@END_Export 
+
+@END_Contents 
